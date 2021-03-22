b05b749509cf45d240c9a4096038e71f8683a9a3
frances87 <77270249+frances87@users.noreply.github.com>
2021-03-22 14:14:36 -0230

    Add files via upload

---
+++ universityApplication.java
@@ -0,0 +1,64 @@
+
+/**
+ * Write a description of class Application here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class universityApplication
+{
+    // instance variables - replace the example below with your own
+    ArrayList<Student> universityStudent;
+    ArrayList<Course> universityCourse;
+    ArrayList<Grades> grades;
+    
+    public universityApplication() {
+        universityCourse = new ArrayList<>();
+        universityStudent = new ArrayList<>();
+        grade = new ArrayList<>();
+    }
+    
+    public boolean addNewStudent(student s){
+        if (!(universityStudent.contain(s))) {
+            universityStudent.add(s);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean addNewCourse(Course x){
+        if (!(universityCourse.contain(x))) {
+            unicversityCourse.add(x);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean enrollForCourse(Student s, Course x) {
+        if (s.enrollForCourse(x) && x.addStudentToCourse(s))
+                 return true;
+        return false;
+    }
+    
+
+    /**
+     * Constructor for objects of class Application
+     */
+    public Application()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
